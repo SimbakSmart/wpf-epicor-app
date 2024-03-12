f7e0261c09535857477efb89cefc93a6c91ce595
Simbak Ramirez <si.ramirez@smart.corp>
2024-03-12 13:26:09 -0600

    02- Nav Bar Primer Version

---
+++ Epicor-Dashboar/Epicor.App/ViewModel/BasicViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Epicor.App.ViewModel
+{
+    public class BasicViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Epicor-Dashboar/Epicor.App/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+
+//using PropertyChanged;
+//using System.Threading.Tasks;
+//using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+
+namespace Epicor.App.ViewModel
+{
+  
+    public class MainViewModel : ObservableObject
+    {
+        
+        public bool IsChecked { get; set; }
+
+
+        public MainViewModel()
+        {
+            IsChecked = false;
+        }
+
+        [RelayCommand]
+        void ToggleCommand()
+        {
+            IsChecked = !IsChecked;  
+        }
+
+        //public ICommand ToggleCommand { get; private set; }
+
+        //public MainViewModel()
+        //{
+        //    IsOpen = false;
+
+        //    //ToggleCommand = new Command
+        //    //(async () => await ToggleMenuAsync());
+        //   // ToggleCommand = new RelayCommand(ToggleMenuAsyn);
+        //}
+        ////private Task<bool> ToggleMenuAsync()
+        //{
+        //   return IsOpen = !IsOpen;
+        //}
+
+    }
+}
