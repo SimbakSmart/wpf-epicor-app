9c048182c17b3bba1302bcc09e0f05472e9b7786
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2024-03-20 17:48:21 -0600

    10 - Se Rediceno la pagina Queue

---
+++ Epicor-Dashboar/Epicor.App/UC/QueueControl - Copy.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows.Controls;
+
+namespace Epicor.App.UC
+{
+   
+    public partial class QueueControl2 : UserControl
+    {
+        public QueueControl2()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
